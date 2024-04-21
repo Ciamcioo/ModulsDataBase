ce72d3e9e66a4fc8d99a98b7a4ad730ba326eda1
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-04-21 12:24:29 +0200

    Updating Junite tests for project

--- app/src/test/java/menu/MainMenuTest.java
+++ app/src/test/java/menu/MainMenuTest.java
@@ -2,8 +2,6 @@ package menu;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainMenuTest {
     static MainMenu menu = MainMenu.getInstance();
@@ -16,14 +14,5 @@ public class MainMenuTest {
     @Test
     public void getInstanceReturnsMenuVariable() {
         assertEquals(menu, MainMenu.getInstance());
-    }
-
-    @Test
-    public void testInitalizationOfMainMenuContent() {
-       ArrayList<String> menuOptions = new ArrayList<>(List.of("1. Something\n", "2. Something else\n", "3. Exit\n"));
-        assertEquals(menuOptions.get(0), menu.getMainMenuOptions().get(0));
-        assertEquals(menuOptions.get(1), menu.getMainMenuOptions().get(1));
-        assertEquals(menuOptions.get(2), menu.getMainMenuOptions().get(2));
-
     }
 }
---
+++ app/src/test/java/menu/MenuCoreTest.java
@@ -0,0 +1,25 @@
+package menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MenuCoreTest {
+    public static MenuCore menu = MainMenu.getInstance(); 
+    
+    @Test
+    public void menuControlerTest() {
+        menu.setPointerPosition(0);
+        assertEquals(false, menu.menuControler('j'));
+        assertEquals(1, menu.getPointerPosition());
+        menu.setPointerPosition(1);
+        assertEquals(false, menu.menuControler('k'));
+        assertEquals(0, menu.getPointerPosition());
+        menu.setPointerPosition(1);
+        assertEquals(true, menu.menuControler('\n'));
+        assertEquals(0, menu.getPointerPosition());
+        menu.setPointerPosition(1);
+        assertEquals(false, menu.menuControler('w'));
+        assertEquals(1, menu.getPointerPosition());
+    } 
+}

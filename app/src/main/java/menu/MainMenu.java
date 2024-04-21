a18ef45416e02ba5c916c13868069ccb7ade7910
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-04-21 12:23:01 +0200

    Disabling echoing to the terminal with the result of key pressed by the user

--- app/src/main/java/menu/MainMenu.java
+++ app/src/main/java/menu/MainMenu.java
@@ -1,14 +1,11 @@
 package menu;
-import java.util.ArrayList;
+
 public final class MainMenu extends MenuCore {
     private static MainMenu instance = null;
-    private static int MAIN_MENU_LIMIT = 2;
     private boolean isUp = true;
-    private ArrayList<String> mainMenuOptions;
 
     private MainMenu() {
-        super(MAIN_MENU_LIMIT);
-        mainMenuOptions = intializeMenu();
+        super(MenuConsts.MAIN_MENU_LIMIT);
     }
 
     // Menu can be wrote based on separete therad 
@@ -17,16 +14,17 @@ public final class MainMenu extends MenuCore {
             printMenu();
             char input = takeUserInput();
             menuControler(input);
+
         } while (isUp);
     }
 
     protected void printMenu() {
-        for (int i = 0; i < mainMenuOptions.size(); i++) {
+        for (int i = 0; i < MenuConsts.MAIN_MENU_OPTIONS.length; i++) {
             if (i == pointerPosition)
                 console.writer().print(">");
             else 
                 console.writer().print(" ");
-            console.writer().print(mainMenuOptions.get(i).toString());  
+            console.writer().print(MenuConsts.MAIN_MENU_OPTIONS[i]);  
             console.flush();
         }
     }
@@ -35,18 +33,5 @@ public final class MainMenu extends MenuCore {
         if (instance == null) 
             instance = new MainMenu();
         return instance;
-    }
-
-
-    private ArrayList<String> intializeMenu() {
-        ArrayList<String> menuOptions = new ArrayList<String>();
-        menuOptions.add("1. Something\n");
-        menuOptions.add("2. Something else\n");
-        menuOptions.add("3. Exit\n");
-        return menuOptions;
-    }
-
-    protected ArrayList<String> getMainMenuOptions() {
-        return this.mainMenuOptions;
     }
 }

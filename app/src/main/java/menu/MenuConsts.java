f7037d7f1c8aa6631766c33d85140d9bf58e726d
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-04-21 12:24:05 +0200

    Creating MenuConst class which will contain all the Consts for classes in package range

---
+++ app/src/main/java/menu/MenuConsts.java
@@ -0,0 +1,9 @@
+package menu;
+
+public final class MenuConsts {
+    // Consts for MainMenu class
+    protected static final int MAIN_MENU_LIMIT = 2;
+    protected static final String[] MAIN_MENU_OPTIONS = {"1. Something\n", "2. Something else\n", "3. Exit\n"};
+}    
+
+
